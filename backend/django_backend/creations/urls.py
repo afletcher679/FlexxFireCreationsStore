a92c4b8809183ae417f2c9bba3d42090c82f267b
Ashley <afletcher679@gmail.com>
2025-12-21 01:32:26 -0500

    set view and url for creation detail

--- backend/django_backend/creations/urls.py
+++ backend/django_backend/creations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreationListCreateAPIView
+from .views import CreationListCreateAPIView, CreationDetailAPIView
 
 urlpatterns = [
     path('creations/', CreationListCreateAPIView.as_view(), name='creation-list'),
+    path('creations/<int:pk>/', CreationDetailAPIView.as_view(), name='creation-detail'),
 ]
--- backend/django_backend/creations/views.py
+++ backend/django_backend/creations/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+# Views are Python functions that take http requests and return http response
+from rest_framework import generics, mixins
 from .models import Creation
 from .serializers import CreationSerializer
 
-class CreationListCreateAPIView(generics.ListCreateAPIView):
+# Gets all the creations and returns them as a list
+class CreationListCreateAPIView(generics.ListAPIView):
     queryset = Creation.objects.all()
     serializer_class = CreationSerializer
+
+# Gets a single creation by primary key and returns it as a dictionary
+class CreationDetailAPIView(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Creation.objects.all()
+    serializer_class = CreationSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
