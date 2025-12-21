b9f802876a9f2ad031b056aa43f9e2efc2d3d9c9
Ashley <afletcher679@gmail.com>
2025-12-21 01:31:56 -0500

    show creation name in admin page

--- backend/django_backend/creations/models.py
+++ backend/django_backend/creations/models.py
@@ -7,3 +7,6 @@ class Creation(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='creations/images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
