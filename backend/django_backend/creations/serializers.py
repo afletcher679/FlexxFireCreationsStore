cc8e7929eae4077314cfc700d1b803512b9025b6
Ashley <afletcher679@gmail.com>
2025-12-21 22:33:32 -0500

    return gallery images when getting creation

--- backend/django_backend/creations/serializers.py
+++ backend/django_backend/creations/serializers.py
@@ -1,7 +1,11 @@
+from gallery_image.serializers import GalleryImageSerializer
 from rest_framework import serializers
 from .models import Creation
 
+
 class CreationSerializer(serializers.ModelSerializer):
+    gallery_images = GalleryImageSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Creation
         fields = '__all__'  # includes all fields like id, name description, price, created_at
