06ab2f5011fe6af59be473d5eedd6bcf5ca8ddde
Ashley <afletcher679@gmail.com>
2025-12-21 23:36:08 -0500

    fix route for detail view of creation

--- frontend/flexx-fire-creations/app/routes.ts
+++ frontend/flexx-fire-creations/app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("catalog", "routes/catalog.tsx"),
-    route("creations/:id", "routes/creations.$id.tsx")
+    route("catalog/creations/:id", "routes/creations.$id.tsx")
 ] satisfies RouteConfig;
