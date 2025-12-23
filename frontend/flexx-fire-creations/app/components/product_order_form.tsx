b173edaa2f451cdec96fe4a897529170e8d8add0
Ashley <afletcher679@gmail.com>
2025-12-22 20:38:29 -0500

    add cursor pointers and format in product_order_form

--- frontend/flexx-fire-creations/app/components/product_order_form.tsx
+++ frontend/flexx-fire-creations/app/components/product_order_form.tsx
@@ -10,19 +10,25 @@ export default function CreationOrderForm({creation} : {creation: Creation}) {
         <>
             <h1 className="text-2xl text-orange-500 font-zalando">{creation.name}</h1>
                 <p className="text-semibold text-xl">${creation.price}</p>
+
+                <ColorOptions colors={creation.colors}/>
+
+                {/* Quantity Buttons */}
                 <div className="flex gap-2 w-1/4">
-                <button className={quantityButtonStyle}
-                disabled={quantity <= 1} 
-                onClick={() => 
-                    setQuantity(prev => Math.max(1, prev - 1))
+                <button className={quantityButtonStyle + (quantity <= 1 ? "cursor-pointer" : "")}
+                    disabled={quantity <= 1} 
+                    onClick={() => 
+                        setQuantity(prev => Math.max(1, prev - 1))
                     }>-</button>
-                <input className="border-solid border border-gray-300 rounded-md p-1 text-center w-1/2" type="number" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value) || 1)}/>
+                <input className="border-solid border border-gray-300 rounded-md p-1 text-center w-1/2" 
+                    type="number" 
+                    value={quantity} 
+                    onChange={(e) => setQuantity(parseInt(e.target.value) || 1)}/>
                 <button className={quantityButtonStyle} onClick={() => setQuantity(prev => prev + 1)}>+</button>
                 </div>
-                <ColorOptions colors={creation.colors}/>
-                <div>
-                <button className="bg-orange-500 text-white p-2 w-full rounded-md active:bg-orange-700">Add to Cart</button>
-                </div>
+
+                <button className="bg-orange-500 text-white p-2 w-full rounded-md active:bg-orange-700 cursor-pointer">Add to Cart</button>
+
                 <p>{creation.description}</p>
         </>
     )
