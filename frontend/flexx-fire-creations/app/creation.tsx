460f1d2e098de3e9ca41d970e8b671bf543234bc
Ashley <afletcher679@gmail.com>
2025-12-22 20:01:22 -0500

    add color interface to creation

--- frontend/flexx-fire-creations/app/creation.tsx
+++ frontend/flexx-fire-creations/app/creation.tsx
@@ -1,12 +1,20 @@
+export interface Color {
+  id: number;
+  name: string;
+  hex_code: string;
+}
+
 export interface GalleryImage {
   id: number;
   image: string;
 }
+
 export interface Creation {
   id: number;
   name: string;
   description: string;
   price: number;
   gallery_images: GalleryImage[];
+  colors: Color[];
   created_at: string;
 };
