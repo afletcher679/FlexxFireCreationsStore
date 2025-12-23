49d2d74430c547d593eccbc75df625fd5f4bb777
Ashley <afletcher679@gmail.com>
2025-12-22 20:38:46 -0500

    be able to select color in color_options

--- frontend/flexx-fire-creations/app/components/color_option.tsx
+++ frontend/flexx-fire-creations/app/components/color_option.tsx
@@ -1,13 +1,16 @@
 import type { Color } from "~/creation";
+import { useState } from "react";
 
 export default function ColorOptions({ colors } : { colors: Color[] }) {
+    const [selectedColor, setSelectedColor] = useState<Color | null>(null);
     return (
         <div className="flex space-x-2">
             {colors && colors.map((color) => (
-                <div
+                <button
                     key={color.id}
-                    className="w-8 h-8 rounded-full border border-gray-300"
+                    className={"w-8 h-8 rounded-full border border-gray-300 cursor-pointer" + (selectedColor?.id === color.id ? " border-3 border-white border-solid" : "")}
                     style={{ backgroundColor: color.hex_code }}
+                    onClick={() => setSelectedColor(color)}
                 />
             ))}
         </div>
