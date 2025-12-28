1970aad8c03f5b6d9f6c6b5bb9ed001ee77d9d51
Ashley <afletcher679@gmail.com>
2025-12-28 16:29:28 -0500

    use fonts and styles from app.css in order form

--- frontend/flexx-fire-creations/app/components/product_order_form.tsx
+++ frontend/flexx-fire-creations/app/components/product_order_form.tsx
@@ -1,6 +1,7 @@
 import type { Creation } from "~/creation";
 import { useState } from "react";
 import ColorOptions from "./color_option";
+import "~/fonts.css";
 
 export default function CreationOrderForm({creation} : {creation: Creation}) {
     const [quantity, setQuantity] = useState(1);
@@ -8,8 +9,9 @@ export default function CreationOrderForm({creation} : {creation: Creation}) {
 
     return (
         <div className="flex flex-col gap-4">
-            <h1 className="text-2xl text-orange-500 font-zalando">{creation.name}</h1>
+            <h1 className="text-2xl text-orange-500 font-zalando-semibold">{creation.name}</h1>
                 <p className="text-semibold text-xl">${creation.price}</p>
+                <p>{creation.mini_description}</p>
 
                 <ColorOptions colors={creation.colors} colorCombos={creation.color_combos}/>
 
@@ -20,16 +22,14 @@ export default function CreationOrderForm({creation} : {creation: Creation}) {
                     onClick={() => 
                         setQuantity(prev => Math.max(1, prev - 1))
                     }>-</button>
-                <input className="border-solid border border-gray-300 rounded-md p-1 text-center w-1/2" 
+                <input className="border-solid border border-gray-300 rounded-md p-1 w-full text-center w-1/2" 
                     type="number" 
                     value={quantity} 
                     onChange={(e) => setQuantity(parseInt(e.target.value) || 1)}/>
                 <button className={quantityButtonStyle} onClick={() => setQuantity(prev => prev + 1)}>+</button>
                 </div>
 
-                <button className="bg-orange-500 text-white p-2 w-full rounded-md active:bg-orange-700 cursor-pointer">Add to Cart</button>
-
-                <p>{creation.description}</p>
+                <button className="btn-orange">Add to Cart</button>  
         </div>
     )
 }
